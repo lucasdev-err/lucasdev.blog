5a3670d02abca1389d1d2b1c5ad4d4b5079999cf
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-06-05 20:56:09 -0300

    Implementacao JWT (Incompleto)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 // Importa o arquivo app.js e o config/env.js
 const app = require("./app");
 const config = require("./config/env");
-const pool = require("./db/db");
-const jwt = require("jsonwebtoken");
-// Importa o módulo pg para criar uma conexão com o banco de dados
 
 app.listen(config.app.port, () => {
     console.log(`Servidor rodando na porta ${config.app.port}`);
